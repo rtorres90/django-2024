912e19a4f6ce05b89cf361945c3fa338d60ce018
Roberto Torres <rtorres@lawlogix.com>
2024-08-26 09:34:55 -0400

    adding images

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import homepage, about
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('about/', about),
     path('posts/', include('posts.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
