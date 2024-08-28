bfb5406f3f660b9aa4067c8f48a0a7f88b9a6f25
Roberto Torres <rtorres@lawlogix.com>
2024-08-28 08:51:25 -0400

    adding user creation

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def user_create(request):
-    return render(request, 'users/user_create.html', {'greetings': "Welcome to user creation page!"})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/user_create.html', {'form':form})
