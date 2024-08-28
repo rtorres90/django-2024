a49e10507d101d9dec8666cd1ac074eb94c4ae14
Roberto Torres <rtorres@lawlogix.com>
2024-08-28 16:14:35 -0400

    last updates and done...

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Steps.
 #. Migrating with ´Python manage.py migrate´
 #. Creating migration from models changes with ´Python manage.py makemigrations´
 #. Creating super user ´python manage.py createsuperuser´ roberto:rto_chile
-
+#. Collecting statics ´python .\manage.py collectstatic´
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 from .views import homepage, about
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', homepage),
     path('about/', about),
@@ -29,4 +32,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
